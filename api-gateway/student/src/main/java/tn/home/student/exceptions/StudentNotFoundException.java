15b61ae955563f7bca0dc55598e381652e24a046
Aymen Ouerghemmi <159343354+AymeenOuerghemmi@users.noreply.github.com>
2025-01-08 20:17:46 +0100

    Delete api-gateway/student directory

--- api-gateway/student/src/main/java/tn/home/student/exceptions/StudentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package tn.home.student.exceptions;
-
-public class StudentNotFoundException extends RuntimeException {
-    public StudentNotFoundException(String message) {
-        super(message);
-    }
-}
--- api-gateway/student/src/main/java/tn/home/student/repositories/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package tn.home.student.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tn.home.student.entities.Student;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
